51edbcb25206280ab27d8574191cf73e4e65936a
jeffads92-spec <jeffads92@gmail.com>
2026-01-08 16:34:22 +0700

    Create cloudinary.php

---
+++ api/config/cloudinary.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Cloudinary\Cloudinary;
+
+return new Cloudinary([
+    'cloud' => [
+        'cloud_name' => getenv('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => getenv('CLOUDINARY_API_KEY'),
+        'api_secret' => getenv('CLOUDINARY_API_SECRET'),
+    ],
+    'url' => [
+        'secure' => true
+    ]
+]);
